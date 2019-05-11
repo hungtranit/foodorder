127ff6f755c9495fb4aca187472656f779d763a3
dongquinguyen <cristiazlatan@gmail.com>
2019-05-12 00:44:55 +0700

    e

--- src/main/java/edu/hcmuaf/food_order/controller/ForumAPI.java
+++ src/main/java/edu/hcmuaf/food_order/controller/ForumAPI.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,27 +15,31 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ForumAPI {
 
-//    @Autowired
-//    QuestionRepository questionRepository;
-//
-//    @GetMapping("/detail-question/{questionID}")
-//    public String getQuestion(Model model, int questionID, Question question) {
-//        model.addAttribute("detail-question", questionRepository.getOne(questionID));
-//        sendDetailQuestion(question);
-//        return "detail-question";
-//    }
-//
-//    @RequestMapping(value = "detail-question", method = RequestMethod.GET)
-//    private ModelAndView sendDetailQuestion(Question question) {
-//        ModelAndView mav = new ModelAndView("detail-question");
-//        mav.addObject("detail-question", question);
-//        return mav;
-//    }
+    @Autowired
+    QuestionRepository questionRepository;
 
-    @GetMapping("detail-question")
-    public String getDetail(Model model) {
+    @GetMapping("/detail-question/{questionID}")
+    public String getQuestion(Model model, @PathVariable int questionID, Question question) {
+        question = questionRepository.getOne(questionID);
+        model.addAttribute("detailquestion", question);
+        sendDetailQuestion(question);
         model.addAttribute("infoUser", new InfoUser());
+        sendUsername(new InfoUser());
         return "detail-question";
+    }
+
+    @RequestMapping(value = "detailquestion", method = RequestMethod.GET)
+    private ModelAndView sendDetailQuestion(Question question) {
+        ModelAndView mav = new ModelAndView("detail-question");
+        mav.addObject("detailquestion", question);
+        return mav;
+    }
+
+    @RequestMapping(value = "infoUser", method = RequestMethod.GET)
+    private ModelAndView sendUsername(InfoUser infoUser) {
+        ModelAndView mav = new ModelAndView("header");
+        mav.addObject("infoUser", infoUser);
+        return mav;
     }
 
 }
