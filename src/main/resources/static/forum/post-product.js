e3cd152e643d474f48dad0808ef2c9bd8c5ccbd5
dongquinguyen <cristiazlatan@gmail.com>
2019-05-27 12:33:04 +0700

    ss

---
+++ src/main/resources/static/forum/post-product.js
@@ -0,0 +1,31 @@
+$(document).submit(function (e) {
+    e.preventDefault();
+    postProduct();
+});
+
+function postProduct() {
+    var product = {}
+    product["typeproduct"] = $('#type-product').val(),
+        product["productname"] = $('#product-name').val(),
+        product["address"] = $('#address').val(),
+        product["phone"] = $('#phone').val(),
+        product["price"] = $('#price').val(),
+        product["decription"] = $('#decription').val(),
+        product["img"] = $('#img').val();
+    $.ajax({
+        type: "POST",
+        url: "/register-user",
+        contentType: "application/json",
+        data: JSON.stringify(infoUser),
+        dataType: 'json',
+        success: function (result) {
+            console.log("result :" + result);
+            window.location.href = "/login";
+        },
+        error: function (e) {
+            console.log(e);
+            window.location.href = "/login";
+        }
+    });
+};
+
